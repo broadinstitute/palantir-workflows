d0e51352e0d4397f40b4b6f3e50c867dd48bcb50
Christopher Kachulis <ckachuli@broadinstitute.org>
2022-09-16 10:16:35 -0400

    renaming, packaging

--- ImputationPipeline/CreateAggregationSets/tests/CreateAggregationSetsTest.py
+++ ImputationPipeline/CreateAggregationSets/tests/CreateAggregationSetsTest.py
@@ -1,9 +1,12 @@
-import ImputationPipeline.CreateSampleSets.CreateSampleSets as CreateSampleSets
+import ImputationPipeline.CreateAggregationSets.CreateAggregationSets as CreateAggregationSets
 import unittest
 import os
 import firecloud.api as fapi
 import json
 import random
+
+current_dir = os.path.dirname(__file__)
+resources_dir = os.path.join(current_dir, "resources")
 
 
 def json_lists_to_sets(o):
@@ -77,16 +80,16 @@ class IntegrationTests(unittest.TestCase):
                                 ]
                                )
 
-        CreateSampleSets.main(self.workspace_namespace, self.workspace_name)
+        CreateAggregationSets.main(self.workspace_namespace, self.workspace_name)
         if expected_sample_sets_json is not None:
             self.assert_expected_sample_sets(expected_sample_sets_json)
         self.assert_all_reworks_false()
 
     def test_simple(self):
-        samples_seq = [("tests/resources/samples.tsv", "tests/resources/expected_sample_sets.json"),
-                       ("tests/resources/samples2.tsv", "tests/resources/expected_sample_sets_2.json"),
-                       ("tests/resources/samples3.tsv", "tests/resources/expected_sample_sets_3.json"),
-                       ("tests/resources/samples4.tsv", "tests/resources/expected_sample_sets_4.json")
+        samples_seq = [(os.path.join(resources_dir, "samples.tsv"), os.path.join(resources_dir, "expected_sample_sets.json")),
+                       (os.path.join(resources_dir, "samples2.tsv"), os.path.join(resources_dir, "expected_sample_sets_2.json")),
+                       (os.path.join(resources_dir, "samples3.tsv"), os.path.join(resources_dir, "expected_sample_sets_3.json")),
+                       (os.path.join(resources_dir, "samples4.tsv"), os.path.join(resources_dir, "expected_sample_sets_4.json"))
                        ]
 
         for samples_tsv, expected_sample_sets_json in samples_seq:
@@ -94,10 +97,10 @@ class IntegrationTests(unittest.TestCase):
                 self.run_test(samples_tsv, expected_sample_sets_json)
 
     def test_sample_sets_delivered(self):
-        samples_seq = [("tests/resources/samples.tsv", "tests/resources/expected_sample_sets.json", []),
-                       ("tests/resources/samples2.tsv", "tests/resources/expected_sample_sets_2_with_delivery.json", ["lb_2"]),
-                       ("tests/resources/samples3.tsv", "tests/resources/expected_sample_sets_3_with_delivery.json", ["lb_1"]),
-                       ("tests/resources/samples4.tsv", "tests/resources/expected_sample_sets_4_with_delivery.json", ["lb_2_group_2"])]
+        samples_seq = [(os.path.join(resources_dir, "samples.tsv"), os.path.join(resources_dir, "expected_sample_sets.json"), []),
+                       (os.path.join(resources_dir, "samples2.tsv"), os.path.join(resources_dir, "expected_sample_sets_2_with_delivery.json"), ["lb_2"]),
+                       (os.path.join(resources_dir, "samples3.tsv"), os.path.join(resources_dir, "expected_sample_sets_3_with_delivery.json"), ["lb_1"]),
+                       (os.path.join(resources_dir, "samples4.tsv"), os.path.join(resources_dir, "expected_sample_sets_4_with_delivery.json"), ["lb_2_group_2"])]
 
         for samples_tsv, expected_sample_sets_json, sample_sets_to_mark_delivered in samples_seq:
             with self.subTest(samples=samples_tsv, expected_samples=expected_sample_sets_json,
@@ -105,21 +108,21 @@ class IntegrationTests(unittest.TestCase):
                 self.run_test(samples_tsv, expected_sample_sets_json, sample_sets_to_mark_delivered)
 
     def test_lone_control(self):
-        self.run_test("tests/resources/samples_lone_control.tsv", "tests/resources/expected_sample_sets_lb_2_only.json")
+        self.run_test(os.path.join(resources_dir, "samples_lone_control.tsv"), os.path.join(resources_dir, "expected_sample_sets_lb_2_only.json"))
 
     def test_no_control(self):
-        self.run_test("tests/resources/samples_no_control.tsv", "tests/resources/expected_sample_sets_lb_2_only.json")
+        self.run_test(os.path.join(resources_dir, "samples_no_control.tsv"), os.path.join(resources_dir, "expected_sample_sets_lb_2_only.json"))
 
     def test_multiple_controls(self):
         with self.assertRaisesRegex(RuntimeError, "Multiple control samples"):
-            self.run_test("tests/resources/samples_multiple_controls.tsv")
+            self.run_test(os.path.join(resources_dir, "samples_multiple_controls.tsv"))
 
     def test_multiple_undelivered(self):
-        self.run_test("tests/resources/samples.tsv")
+        self.run_test(os.path.join(resources_dir, "samples.tsv"))
         # deliver lb_1, add new samples
-        self.run_test("tests/resources/samples2.tsv", sample_sets_to_mark_delivered=["lb_1"])
+        self.run_test(os.path.join(resources_dir, "samples2.tsv"), sample_sets_to_mark_delivered=["lb_1"])
         # unmark delivery of lb_1
-        fapi.update_entity(self.workspace_namespace, self.workspace_name, "tests/resources/sample_set", "lb_1",
+        fapi.update_entity(self.workspace_namespace, self.workspace_name, "sample_set", "lb_1",
                            [{"op": "AddUpdateAttribute",
                              "attributeName": "delivered",
                              "addUpdateAttribute": False}
@@ -129,12 +132,12 @@ class IntegrationTests(unittest.TestCase):
             self.run_test()
 
     def test_rework(self):
-        samples_seq = [("tests/resources/samples.tsv", "tests/resources/expected_sample_sets.json", [], ["sample_id_1"]),
+        samples_seq = [(os.path.join(resources_dir, "samples.tsv"), os.path.join(resources_dir, "expected_sample_sets.json"), [], ["sample_id_1"]),
                        # sample_id_1 marked as rework, but not yet in sample set.
                        # should ignore rework and treat normally
-                       (None, "tests/resources/expected_sample_sets.json", [], ["sample_id_1"]),
+                       (None, os.path.join(resources_dir, "expected_sample_sets.json"), [], ["sample_id_1"]),
                        # mark sample_id_1 as reworked, but don't yet deliver, result is same
-                       (None, "tests/resources/expected_sample_sets_rework.json", ["lb_1"], ["sample_id_1"]),
+                       (None, os.path.join(resources_dir, "expected_sample_sets_rework.json"), ["lb_1"], ["sample_id_1"]),
                        # deliver lb_1, now reworking sample_id_1 leads to additional aggregation set
                        ]
         for samples_tsv, expected_sample_sets_json, sample_sets_to_mark_delivered, samples_to_mark_rework in samples_seq:
@@ -146,11 +149,11 @@ class IntegrationTests(unittest.TestCase):
 
     def test_rework_no_control(self):
         # the fact that the sample marked as rework should have no effect when there is no control sample
-        self.run_test("tests/resources/samples_no_control.tsv", "tests/resources/expected_sample_sets_lb_2_only.json", samples_to_mark_rework="sample_id_1")
+        self.run_test(os.path.join(resources_dir, "samples_no_control.tsv"), os.path.join(resources_dir, "expected_sample_sets_lb_2_only.json"), samples_to_mark_rework="sample_id_1")
 
     def test_rework_and_new_mixed(self):
-        samples_seq = [("tests/resources/samples.tsv", "tests/resources/expected_sample_sets.json", [], []),
-                       ("tests/resources/samples2.tsv", "tests/resources/expected_sample_sets_2_mix_rework_and_new.json", ["lb_2"], ["sample_id_5"]) # deliver lb_2, rework sample 5
+        samples_seq = [(os.path.join(resources_dir, "samples.tsv"), os.path.join(resources_dir, "expected_sample_sets.json"), [], []),
+                       (os.path.join(resources_dir, "samples2.tsv"), os.path.join(resources_dir, "expected_sample_sets_2_mix_rework_and_new.json"), ["lb_2"], ["sample_id_5"]) # deliver lb_2, rework sample 5
                        ]
         for samples_tsv, expected_sample_sets_json, sample_sets_to_mark_delivered, samples_to_mark_rework in samples_seq:
             with self.subTest(samples=samples_tsv, expected_samples=expected_sample_sets_json,
