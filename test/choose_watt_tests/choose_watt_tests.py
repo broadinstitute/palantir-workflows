99db135179297a055ada056e489687a7864f225f
Chris Kachulis <ckachuli@broadinstitute.org>
2024-08-14 12:05:51 -0400

    AggregatePRSResults tests should run due to wdl change

--- test/choose_watt_tests/choose_watt_tests.py
+++ test/choose_watt_tests/choose_watt_tests.py
@@ -5,6 +5,7 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--changed-workflows", help="Name of workflow(s) which have changed", nargs='*')
+parser.add_argument("--changed-jsons", help="Name of json(s) which have changed", nargs='*')
 parser.add_argument("--watt-config", help="WATT config yaml")
 parser.add_argument("--womtool-jar")
 
@@ -53,19 +54,24 @@ if __name__ == '__main__':
     
     tests_to_run = set()
     
-    if args.changed_workflows: 
+    if args.changed_workflows or args.changed_jsons: 
         changed_wdls = {resolve_relative_path(path) for path in args.changed_workflows}
+        changed_jsons = {resolve_relative_path(path) for path in args.changed_jsons}
 
-        for test, test_info in config.items():
-            wdl_path = resolve_relative_path(test_info['path'])
+        for wf, wf_tests_info in config.items():
+            wdl_path = resolve_relative_path(wf_tests_info['path'])
             if wdl_path in changed_wdls:
-                tests_to_run.add(test)
+                tests_to_run.add(wf)
             else:
+                for test, test_info in wf_tests_info['tests']:
+                    if resolve_relative_path(test_info['test_inputs']) in changed_jsons or resolve_relative_path(test_info['expected_outputs']) in changed_jsons:
+                        tests_to_run.add(wf)
+            if wf not in tests_to_run:
                 womtool_run = subprocess.run(['java', "-jar", args.womtool_jar, "validate", "-l", wdl_path], capture_output=True)
                 dependencies = get_wdl_dependencies(womtool_run)
                 for dependency in dependencies:
                     if dependency in changed_wdls:
-                        tests_to_run.add(test)
+                        tests_to_run.add(wf)
                         break
         
-        print(" ".join(tests_to_run))
+    print(" ".join(tests_to_run))
