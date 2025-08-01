3d37676ebdd1a8130d68e9b2a71d25dc39c72341
Michael Gatzen <mgatzen@broadinstitute.org>
2025-08-01 15:55:50 -0400

    Defining thresholds and labels in resource TSV file

--- BGESexEstimation/BGESexEstimation.py
+++ BGESexEstimation/BGESexEstimation.py
@@ -1,6 +1,39 @@
 import argparse
 import csv
 
+class ThresholdsAndLabels:
+    def __init__(self, lower_bound_xx, upper_bound_xx, lower_bound_xy, upper_bound_xy, label_xx, label_xy, label_inconclusive):
+        self.lower_bound_xx = lower_bound_xx
+        self.upper_bound_xx = upper_bound_xx
+        self.lower_bound_xy = lower_bound_xy
+        self.upper_bound_xy = upper_bound_xy
+        self.label_xx = label_xx
+        self.label_xy = label_xy
+        self.label_inconclusive = label_inconclusive
+
+def read_thresholds_and_labels(thresholds_and_labels_path):
+    with open(thresholds_and_labels_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        line = next(reader)
+        if line is None:
+            raise RuntimeError("Thresholds and labels file is empty.")
+        if not all(key in line for key in ['lower_bound_xx', 'upper_bound_xx', 'lower_bound_xy', 'upper_bound_xy', 'label_xx', 'label_xy', 'label_inconclusive']):
+            raise RuntimeError("Thresholds and labels file is missing required fields.\n" + 
+                               "Expected fields: lower_bound_xx, upper_bound_xx, lower_bound_xy, upper_bound_xy, label_xx, label_xy, label_inconclusive\n" + 
+                               "Found fields:    " + ', '.join(line.keys()))
+        thresholds_and_labels = ThresholdsAndLabels(
+            lower_bound_xx=float(line['lower_bound_xx']),
+            upper_bound_xx=float(line['upper_bound_xx']),
+            lower_bound_xy=float(line['lower_bound_xy']),
+            upper_bound_xy=float(line['upper_bound_xy']),
+            label_xx=line['label_xx'],
+            label_xy=line['label_xy'],
+            label_inconclusive=line['label_inconclusive']
+        )
+        if next(reader, None) is not None:
+            raise RuntimeError("Thresholds and labels file contains more than one line.")
+        return thresholds_and_labels
+
 def read_coverage_metrics(metrics_file_path):
     covX = None
     covY = None
@@ -14,30 +47,22 @@ def read_coverage_metrics(metrics_file_path):
                 covY = float(row['mean_coverage'])
     return covX, covY
 
-def estimate_sex_ploidy(covX, covY):
-    lower_bound_xx = 0
-    upper_bound_xx = 0.05
-    lower_bound_xy = 0.25
-    upper_bound_xy = 0.8
-
-    label_xx = 'F'
-    label_xy = 'M'
-    label_inconclusive = 'U'
-
+def estimate_sex_ploidy(covX, covY, thresholds_and_labels):
     if covX is None or covY is None:
         raise RuntimeError("Coverage metrics for X or Y chromosome not found.")
     
     if covX == 0 or covY == 0:
-        return 0, label_inconclusive
+        return 0, thresholds_and_labels.label_inconclusive
     
     ratio = covY / covX
 
-    estimated_sex_ploidy = label_xx if lower_bound_xx <= ratio <= upper_bound_xx else (label_xy if lower_bound_xy <= ratio <= upper_bound_xy else label_inconclusive)
+    estimated_sex_ploidy = thresholds_and_labels.label_xx if thresholds_and_labels.lower_bound_xx <= ratio <= thresholds_and_labels.upper_bound_xx else (thresholds_and_labels.label_xy if thresholds_and_labels.lower_bound_xy <= ratio <= thresholds_and_labels.upper_bound_xy else thresholds_and_labels.label_inconclusive)
     return ratio, estimated_sex_ploidy
 
-def main(metrics_file_path):
+def main(metrics_file_path, thresholds_and_labels_path):
+    thresholds_and_labels = read_thresholds_and_labels(thresholds_and_labels_path)
     covX, covY = read_coverage_metrics(metrics_file_path)
-    ratio, estimated_sex_ploidy = estimate_sex_ploidy(covX, covY)
+    ratio, estimated_sex_ploidy = estimate_sex_ploidy(covX, covY, thresholds_and_labels)
 
     with open('sex_ploidy_ratio.txt', 'w') as sex_ploidy_ratio_file:
         sex_ploidy_ratio_file.write(str(ratio) + '\n')
@@ -48,10 +73,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="BGESexEstimation: Estimate sample sex ploidy from exome region coverage metrics file",
     )
-    parser.add_argument(
-        "coverage_metrics",
-        type=str,
-        help="Path to the exome region coverage metrics file."
-    )
+    parser.add_argument('--coverage-metrics', type=str, help='Path to the exome region coverage metrics file.', required=True)
+    parser.add_argument('--thresholds-and-labels', type=str, help='Path to the thresholds and labels file.', required=True)
+
     args = parser.parse_args()
-    main(args.coverage_metrics)
+    main(args.coverage_metrics, args.thresholds_and_labels)
